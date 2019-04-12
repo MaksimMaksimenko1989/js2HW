01e99e51290e7bfe8a80b98acf85b9abfdda769a
Maxim <maks16082013@gmail.com>
2019-04-12 07:55:19 +0400

    ff

--- javascript.js
+++ javascript.js
@@ -35,6 +35,29 @@ console.log(messageIN);
 
 
 
+var month = 8;
+
+var winter = (month <=2 && month ===12) ? 'true' : 'false';
+console.log(winter);
+var spring = (month >= 3 && month <=5) ? 'true' : 'false';
+console.log(spring);
+var summer = (month >= 6 && month <=8) ? 'true' : 'false';
+console.log(summer);
+var autumn = (month >= 9 && month <=11) ? 'true' : 'false';
+console.log(autumn);
+
+var numbers = 100;
+
+for (var i = 0; i < numbers; i+=1) {
+  if (i % 2 == 0) {
+    console.log('numb: ' + i);
+  }
+}
+
+
+
+
+
 
 
 
